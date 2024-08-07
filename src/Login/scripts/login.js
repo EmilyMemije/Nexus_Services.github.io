5584c73a366f98c7fe32ce872e8ff36f2fbf1e88
Josh07791 <jos_hernandezc03@hotmail.com>
2024-08-07 10:08:58 -0600

    Actualizacion del login

--- src/Login/scripts/login.js
+++ src/Login/scripts/login.js
@@ -2,16 +2,16 @@
 const buttonLogin = document.getElementById('login');
 buttonLogin.disabled = true;
 
-/** Expresiones regulares (Regex)
- * Patrones de caracteres que se deben cumplir para realizar una acción.
- * Podemos validad expresiones regulares en cualquier lenguaje de programación
- */
-// Crear regex para un correo con la forma email @gmail.com que puede recibir cualquier letra mayúscula o minúscula, cualquier número y los caracteres `.-_*`
-const emailRegex = /^[a-zA-Z0-9._\*\-]+@gmail\.com$/; /*   /vive mi expresion regular/   */
+//Expresion regular
+const emailGmailRegex = /^[a-zA-Z0-9._\*\-]+@gmail\.com$/;
+const emailHotmailRegex = /^[a-zA-Z0-9._\*\-]+@hotmail\.com$/;
+
+const passwordRegex = /^[a-zA-Z0-9$%&_\@#!.\-]{8,}$/;
 
 
 // Mando a llamar el input para poder evaluarlo y cumplir con la regex
-const emailInput = document.getElementById('email');
+const emailInput = document.getElementById('email-login');
+const passwordInput = document.getElementById('password--login');
 
 // Crear un evento para el input, el cual va a evaluar el patron regex y en caso de cumplir con el patron el boton se habilitara.
 
@@ -19,8 +19,17 @@ emailInput.addEventListener('input', (event) =>{
     const email = event.target.value;
 
     // Sentencia condicional para evaluar el valor del input, si se cumple se habilita el boton, en caso contrario, sigue deshabilitado
-    if(emailRegex.test(email) === true){
-        buttonLogin.disabled = false;
+    if(emailGmailRegex.test(email) === true || emailHotmailRegex.test(email) === true ){
+        passwordInput.addEventListener('input', (event) =>{
+            const password = event.target.value;
+        
+            // Sentencia condicional para evaluar el valor del input, si se cumple se habilita el boton, en caso contrario, sigue deshabilitado
+            if(passwordRegex.test(password) === true ){
+                buttonLogin.disabled = false;
+            } else {
+                buttonLogin.disabled = true;
+            }
+        });
     } else {
         buttonLogin.disabled = true;
     }

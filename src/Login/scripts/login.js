68c05f8958162404d0e99dff06ba586d9373e1e6
Josh07791 <jos_hernandezc03@hotmail.com>
2024-08-08 17:49:29 -0600

    Actualizacion del login, JSON

--- src/Login/scripts/login.js
+++ src/Login/scripts/login.js
@@ -12,6 +12,7 @@ const passwordRegex = /^[a-zA-Z0-9$%&_\@#!.\-]{8,}$/;
 // Mando a llamar el input para poder evaluarlo y cumplir con la regex
 const emailInput = document.getElementById('email-login');
 const passwordInput = document.getElementById('password--login');
+const form = document.getElementById('form--login');
 
 // Crear un evento para el input, el cual va a evaluar el patron regex y en caso de cumplir con el patron el boton se habilitara.
 
@@ -33,4 +34,45 @@ emailInput.addEventListener('input', (event) =>{
     } else {
         buttonLogin.disabled = true;
     }
+});
+
+class usuario{
+    constructor(correo, contraseña){
+        this.email = correo;
+        this.password = contraseña;
+    }
+}
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    fetch(`login.json`)
+        .then(response => response.json())
+        .then(data => {
+            let loginExitoso = false;
+            data.forEach(usuarioData =>{ 
+                //Se aguarda los datos del JSON al las variables correo, contraseña}
+                const { correo, contraseña} = usuarioData;
+                //Se aguarda la informacion obtenida del JSO en la Clase Usuario
+                const usuarioLogin = new usuario( correo, contraseña);
+                
+                // Aqui se hace la comparacion de lo que tenemos en la clase con el valor de input
+                if(usuarioLogin.email === emailInput.value && usuarioLogin.password === passwordInput.value ){
+                    loginExitoso = true;
+                    alert("Se hizo login");
+                    //ya que se cumple la condicion hace el action del form
+                    event.target.submit();
+                }
+            })
+            if (!loginExitoso) {
+                //si el login no es correcto entonces se limpia los inputs
+                alert("Correo o contraseña incorrecto");
+                emailInput.value = '';
+                passwordInput.value = '';
+                buttonLogin.disabled = true;
+            }
+        })
+        .catch(error => {
+            alert (`Hubo un error al cargar los datos`);
+            console.error(`Èrror el cargar`, error);
+        });
 });

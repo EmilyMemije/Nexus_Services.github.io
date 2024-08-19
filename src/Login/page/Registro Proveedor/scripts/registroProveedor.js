530d1088cec4e4109ddc936cf548d03047ca2864
liceajj <jlicea481@gmail.com>
2024-08-18 20:50:53 -0600

    Se agrega al js de registroProvedor el codigo para guardar en el localStorage la informacion del formulario

--- src/Login/page/Registro Proveedor/scripts/registroProveedor.js
+++ src/Login/page/Registro Proveedor/scripts/registroProveedor.js
@@ -124,3 +124,17 @@ fetch(`registro.json`, {
 
 });
 
+const procesaTodo = (event)=>{
+    event.preventDefault();
+    const datos = new FormData(event.target);
+    
+    const datosCompletos = Object.fromEntries(datos.entries());
+    console.log(JSON.stringify(datosCompletos));
+}
+
+registrarseForm.addEventListener('submit', procesaTodo);
+
+
+
+
+    

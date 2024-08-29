9d6d0ccbeab09703c0f9f8997ebc86d9ad342352
RobVen463 <venegasr6@gmail.com>
2024-08-29 01:32:28 -0600

    cambio al 90 de las nav bar y footer

--- src/Login/page/Perfil Usuario/scripts/cierreSesion.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('cerrarSesion').addEventListener('click', function() {
-    // Limpiar el localStorage
-    // localStorage.removeItem('usuarioLogueado');
-    // localStorage.removeItem('nombreActualizado');
-    // localStorage.removeItem('emailActualizado');
-    // localStorage.removeItem('telefonoActualizado');
-    // localStorage.removeItem('correoActualizado');
-    localStorage.clear();
-});
